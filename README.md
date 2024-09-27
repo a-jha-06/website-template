dc3a173c43d0e32e75f66b70411f682de0f769a2
Amisha Jha <amishajha06@gmail.com>
2024-09-27 23:59:30 +0530

    route paths added

---
+++ README.md
@@ -0,0 +1,38 @@
+#EcultifyAssignment
+
+#Custom website developed to complete assessment for FrontEnd Developer role in Ecultify Advertising.
+
+## Table of Contents
+- Installation
+- Usage
+- Features
+- Technologies Used
+- Contact
+
+
+##Installation
+
+- npm i
+- npm run start
+
+
+##Usage
+- Custom website developed to complete assessment test given by Ecultify Advertising organization to test FrontEnd developer skills.
+- Website can be configured to showcase products, digital marketing.
+
+##Features
+- Navigation bars are added to help users navigate between different tabs.
+- Lightning cards are added to showcase different product images on the page
+- Testimonial cards are added to showcase different product images on the page
+- Footer is added with the developer information
+- Scrolling is enabled 
+- The page is responsive to all devices.
+
+##Technologies used
+- ReactJs
+- CSS, SASS
+- Bootstrap 
+
+##Contact
+- amishajha06@gmail.com
+- https://www.linkedin.com/in/amisha-jha-06999s/
--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Pages/navbar/navbar'; // Adjusted the path
-import Home from './Pages/home'; // Adjust the paths as per your folder structure
+import Navbar from './Pages/navbar/navbar'; // Adjust paths based on your folder structure
+import Home from './Pages/home';
 import Login from './Pages/login';
 import Products from './Pages/products';
 import Footer from './components/Footer/footer';
-
 import HeroBanner from './components/Banner/banner';
 import Solution from './Pages/solution';
 import Community from './Pages/community';
@@ -13,32 +12,32 @@ import Resources from './Pages/resources';
 import Contact from './Pages/contact';
 import Register from './Pages/register';
 import Pricing from './Pages/pricing';
+
 function App() {
   return (
-    <div >
-      <Router>
-        <div className='container'>
-          <Navbar />
-          <HeroBanner />
-        </div>
+    <Router>
+      <div className='container'>
+        {/* Navbar and HeroBanner inside the container */}
+        <Navbar />
+        <HeroBanner />
 
+        {/* Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
-          
-          <Route path="/products" element={<Products/>} /> {/* lowercase path to ensure uniformity */}
-          <Route path="/solution" element={<Solution/>} /> {/* lowercase path to ensure uniformity */}
-          <Route path="/community" element={<Community />} /> {/* lowercase path to ensure uniformity */}
-          <Route path="/resources" element={<Resources />} /> {/* lowercase path to ensure uniformity */}
-          <Route path="/pricing" element={<Pricing />} /> {/* lowercase path to ensure uniformity */}
-
-          <Route path="/contact" element={<Contact />} /> {/* lowercase path to ensure uniformity */}
-          <Route path="/signin" element={<Login />} /> {/* lowercase path to ensure uniformity */}
-          <Route path="/register" element={<Register />} /> {/* lowercase path to ensure uniformity */}
+          <Route path="/products" element={<Products />} />
+          <Route path="/solution" element={<Solution />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
 
-        <Footer /> {/* Footer outside the Routes */}
-      </Router>
-    </div>
+        {/* Footer inside the container */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
